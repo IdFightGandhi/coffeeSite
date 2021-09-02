d7e4736f0a2f4a143fb5d8b1e2a1d89a152a022d
Jeffery Patterson <jpatterson933@ucla.edu>
2021-09-02 11:55:45 -0700

    removed dependencies from pack-json in the client folder (react app) that we will not be using since we are not setting up any tests. Also installed react-icons that will be used for carousel

--- coffeesite/src/App.js
+++ coffeesite/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import CarouselSlider from './components/Carousel/Carousel';
+import { CarouselData } from './components/Carousel/CarouselData';
 
 function App() {
   return (
     <div>
       <h1>Coffe Shop</h1>
-      <CarouselSlider />
+      <CarouselSlider slides={CarouselData} />
 
 
     </div>
--- coffeesite/src/components/Carousel/Carousel.js
+++ coffeesite/src/components/Carousel/Carousel.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { CarouselData } from './CarouselData';
+import { FaArrowAltCircleRight, FaArrowAltCircleLeft } from 'react-icons/fa';
 
-const CarouselSlider = () => {
+const CarouselSlider = ({ slides }) => {
+    // set our state
+    const [current, setCurrent] = useState(0)
+    const length = slides.length
+
+
     return (
         <div>
-            <>
+            <section className="slider">
+                <FaArrowAltCircleLeft className="left-arrow" />
+                <FaArrowAltCircleRight className="right-arrow" />
             {CarouselData.map((slide, index) => {
                 return <img src={slide.image} alt='testing' />;
             })}
-         </>
+         </section>
         </div>
     )
 }
