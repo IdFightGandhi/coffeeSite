4cc0084b3656b99884802f06cdf020ad3d6e37ca
Jeffery Patterson <jpatterson933@ucla.edu>
2021-09-05 19:29:09 -0700

    reset carousle. Tyring a new approach

--- coffeesite/src/App.js
+++ coffeesite/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import { Carousel } from './components/Carousel/Carousel'
 
 function App() {
   return (
     <div>
       <h1>Coffee Shop</h1>
-      <Carousel />
     </div>
   );
 }
