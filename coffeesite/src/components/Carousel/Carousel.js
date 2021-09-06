1f9e75c718bdb74a8e355953366e865e2e95df7f
Jeffery Patterson <jpatterson933@ucla.edu>
2021-09-06 10:03:40 -0700

    added images to carousel. carousel now auotmatically changes and can be manually switch by users

--- coffeesite/src/components/Carousel/Carousel.js
+++ coffeesite/src/components/Carousel/Carousel.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Carousel from 'react-elastic-carousel';
 import Image from './Image';
+import ImageData from './ImageData';
 
 //these breakpoints have the image reach according to the size of the screen
 const breakPoints = [
@@ -10,20 +11,35 @@ const breakPoints = [
     { width: 1200, itemsToShow: 4 }
 ];
 
+
+
+
+
 export const Slideshow = () => {
+
+    const Data = ImageData.map((slide,index) => {
+       
+        return (
+            <img src={slide.image} alt={slide.alt} key={slide.image}></img>
+        )
+    })
+    console.log(Data)
+
     return (
         <div>
         <h1 style={{ textAlign: "center"}}>Example</h1>
+        
         <div className="carousel">
-            <Carousel breakPoints={breakPoints} enableAutoPlay='true' autoPlaySpeed='2000'>
-                <Image>One</Image>
-                <Image>Two</Image>
-                <Image>Three</Image>
-                <Image>Four</Image>
-                <Image>Five</Image>
-                <Image>Six</Image>
-                <Image>Seven</Image>
-                <Image>Eight</Image>
+            <Carousel breakPoints={breakPoints} enableAutoPlay='true' autoPlaySpeed='3500'>
+                
+                <Image>{Data[0]}</Image>
+                <Image>{Data[1]}</Image>
+                <Image>{Data[2]}</Image>
+                <Image>{Data[3]}</Image>
+                <Image>{Data[4]}</Image>
+                <Image>{Data[5]}</Image>
+                <Image>{Data[6]}</Image>
+                <Image>{Data[7]}</Image>
             </Carousel>
         </div>
         </div>
---
+++ coffeesite/src/components/Carousel/ImageData.js
@@ -0,0 +1,36 @@
+const ImageData = [
+    {
+        image: 'https://www.adityabirlacapital.com/healthinsurance/active-together/wp-content/uploads/2019/12/How-Are-Espressos-Good-For-Health_blog-1.gif',
+        alt: 'This is a test image of coffe'
+    },
+    {
+        image: 'https://www.calvinescoffee.com/wp-content/uploads/2020/07/Cafe-Latte-Calvines-Coffee-1140x450.jpg',
+        alt: 'This is a test image of coffe'
+    },
+    {
+        image: 'https://www.acouplecooks.com/wp-content/uploads/2020/10/How-to-make-an-Americano-004.jpg',
+        alt: 'This is a test image of coffe'
+    },
+    {
+        image: 'https://www.acouplecooks.com/wp-content/uploads/2020/10/how-to-make-a-macchiato-003.jpg',
+        alt: 'This is a test image of coffe'
+    },
+    {
+        image: 'https://www.acouplecooks.com/wp-content/uploads/2020/10/how-to-make-cappuccino-005.jpg',
+        alt: 'This is a test image of coffe'
+    },
+    {
+        image: 'https://www.acouplecooks.com/wp-content/uploads/2021/04/Flat-White-003-1.jpg',
+        alt: 'This is a test image of coffe'
+    },
+    {
+        image: 'https://www.acouplecooks.com/wp-content/uploads/2021/08/Cortado-004.jpg',
+        alt: 'This is a test image of coffe'
+    },
+    {
+        image: 'https://www.acouplecooks.com/wp-content/uploads/2020/10/How-to-Make-a-Mocha-002.jpg',
+        alt: 'This is a test image of coffe'
+    }
+]
+
+export default ImageData;
