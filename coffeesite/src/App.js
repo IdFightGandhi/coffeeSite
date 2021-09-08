d620b7455820fd951bcdc0e52195d34ed5e6908e
Jeffery Patterson <jpatterson933@ucla.edu>
2021-09-08 14:10:52 -0700

    new carousel

--- coffeesite/src/App.js
+++ coffeesite/src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
 import Header from './components/Header'
 import TopNav from './components/TopNav'
-import { Slideshow } from './components/Carousel/Carousel';
+import { LandingSlider } from './components/Slider';
 
 function App() {
   return (
     <div>
       <Header id="black-border" />
       <TopNav id="topnav"/>
-
-
-      <h1>Coffee Shop</h1>
-      <Slideshow />
+      <LandingSlider />
     </div>
 
   );
