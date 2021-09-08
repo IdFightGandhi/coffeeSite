7bb0045e6ba58e7cfba94ba856c81c3d11d02867
William Thorne <williamcthorne@gmail.com>
2021-09-08 14:05:52 -0700

    subnav routingg start, 09-08 work pre-merge

--- coffeesite/src/components/Sidebar/index.js
+++ coffeesite/src/components/Sidebar/index.js
@@ -1,6 +1,4 @@
 import React, {useState} from 'react'
-import styled from 'styled-components'
-import {Link} from 'react-router-dom'
 import * as FaIcons from 'react-icons/fa'
 import * as AiIcons from 'react-icons/ai'
 import {
